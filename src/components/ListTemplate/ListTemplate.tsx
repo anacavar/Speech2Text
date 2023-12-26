b98683231f54901cdcc32479e15bb1f9ad4a9297
Ana Cavar <ana.cavar22@gmail.com>
2023-12-26 22:27:17 +0100

    css fixes

--- src/components/ListTemplate/ListTemplate.tsx
+++ src/components/ListTemplate/ListTemplate.tsx
@@ -6,12 +6,14 @@ const ListTemplate = ({ content }) => {
     <div className="s2t-list-template">
       <h1>{content.title}</h1>
       <p>{content.text}</p>
-      <input
-        className="s2t-search"
-        type="text"
-        placeholder="Type to search..."
-      />
-      <Popup popupContent={content.popupContent} />
+      <div className="s2t-list-functionalities">
+        <input
+          className="s2t-search"
+          type="text"
+          placeholder="Type to search..."
+        />
+        <Popup popupContent={content.popupContent} />
+      </div>
       {content.list}
     </div>
   );
